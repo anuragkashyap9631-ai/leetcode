c15e200a05071257e6da64685434394fa40f6305
anuragkashyap9631-ai <anuragkashyap9631@gmail.com>
2026-09-01 02:07:42 +0530

    Time: 0 ms (100%), Space: 31.2 MB (81.5%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -8,7 +8,7 @@ public:
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[mid]<target){
+           else if(nums[mid]<target){
                 s= mid + 1;
             }
             else{
